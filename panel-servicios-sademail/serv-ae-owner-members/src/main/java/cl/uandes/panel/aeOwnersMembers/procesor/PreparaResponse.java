519d22a91e9d2619656c575337357f9a21217585
Fernando Ibarra <fibarra.fir@gmail.com>
2023-03-03 20:01:46 -0300

    al 03-03-2023

--- panel-servicios-sademail/serv-ae-owner-members/src/main/java/cl/uandes/panel/aeOwnersMembers/procesor/PreparaResponse.java
+++ panel-servicios-sademail/serv-ae-owner-members/src/main/java/cl/uandes/panel/aeOwnersMembers/procesor/PreparaResponse.java
@@ -1,9 +1,12 @@
 package cl.uandes.panel.aeOwnersMembers.procesor;
 
+import java.util.Map;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.ExchangeBuilder;
 import org.apache.cxf.message.MessageContentsList;
 import org.apache.log4j.Logger;
 
@@ -24,20 +27,38 @@ public class PreparaResponse implements Processor {
 	
 	@Override
 	public void process(Exchange exchange) throws Exception {
+		/*
 		MessageContentsList list = (MessageContentsList)exchange.getIn().getBody();
 		logger.info(String.format("list.size=%d", list.size()));
 		PanelEsbResponseTYPE responseTYPE = (PanelEsbResponseTYPE) list.get(0);
 		Integer codigo = responseTYPE.getCodigo();
 		String glosa = responseTYPE.getGlosa();
 		exchange.getIn().setHeader("glosaWS_JBOSS", glosa);
+		*/
+		Integer codigo = 0;
+		String glosa = "Activado proceso que agrega/elimina owners";
+		exchange.getIn().setHeader("glosaWS_JBOSS", glosa);
+		
 		
 		// Activa la parte batch si el WS respondio que estaba trabajando
 		if (codigo == 0) {
 			CamelContext camelContext = exchange.getContext();
 			ProducerTemplate apiProcesoBatch = camelContext.createProducerTemplate();
-			apiProcesoBatch.requestBodyAndHeaders("seda:procesoBatch?timeout=-1",
-					exchange.getIn().getBody(), exchange.getIn().getHeaders());
+			Exchange batchExchange = ExchangeBuilder.anExchange(camelContext).build();
+			batchExchange.setMessage(exchange.getIn());
+			
+			logger.info(String.format("batchExchange.body: %s keys en header: %s", batchExchange.getIn().getBody(),
+					dumpHeaders(batchExchange.getIn().getHeaders())));
+			apiProcesoBatch.asyncSend("seda:procesoBatch?timeout=-1", batchExchange);
 		}
+	}
+
+	private Object dumpHeaders(Map<String, Object> headers) {
+		StringBuffer sb = new StringBuffer();
+		for (String key : headers.keySet()) {
+			sb.append(String.format("%s - %s", key, headers.get(key)));
+		}
+		return sb.toString();
 	}
 
 }
