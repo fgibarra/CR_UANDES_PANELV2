bbfcaecf89dccb38774c3d19a419c854c1a2f2d4
fgibarra <fibarra.fir@gmail.com>
2023-11-17 21:36:15 -0300

    Panel V2 migracion a 11-17-2023

--- panel-servicios-sademail/servicios-batch-panel/crear-grupos-batch/src/main/resources/sql/getIdsMiembroGrupos.sql
+++ panel-servicios-sademail/servicios-batch-panel/crear-grupos-batch/src/main/resources/sql/getIdsMiembroGrupos.sql
@@ -1,3 +1,3 @@
 select c.id_gmail, g.group_id, n.id_miembro, n.key_grupo, c.login_name
-from nap_grupo_miembro n, mi_cuentas c, mi_grupos g
+from nap_grupo_miembro n, mi_cuentas_gmail c, mi_grupos g
 where c.moodle_id=n.id_miembro and g.key=n.key_grupo and n.activo=:#tipoOperacion and n.key_grupo=:#keyGrupo
