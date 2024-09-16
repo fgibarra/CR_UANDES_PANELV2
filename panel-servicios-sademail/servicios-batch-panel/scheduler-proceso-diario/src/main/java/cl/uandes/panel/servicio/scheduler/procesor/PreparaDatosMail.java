6602f766dde178e6301da56b3f253803faa2420f
fgibarra <fibarra.fir@gmail.com>
2024-09-16 14:06:35 -0300

    Panel V2 migracion a 09-16-2024

--- panel-servicios-sademail/servicios-batch-panel/scheduler-proceso-diario/src/main/java/cl/uandes/panel/servicio/scheduler/procesor/PreparaDatosMail.java
+++ panel-servicios-sademail/servicios-batch-panel/scheduler-proceso-diario/src/main/java/cl/uandes/panel/servicio/scheduler/procesor/PreparaDatosMail.java
@@ -1,7 +1,10 @@
 package cl.uandes.panel.servicio.scheduler.procesor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
@@ -9,6 +12,7 @@ import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.PropertyInject;
+import org.apache.log4j.Logger;
 
 import cl.uandes.panel.comunes.json.sendmail.SendmailRequest;
 import cl.uandes.panel.comunes.servicios.dto.SendmailParamsDTO;
@@ -26,18 +30,68 @@ public class PreparaDatosMail implements Processor {
 	private String asunto;
 	@PropertyInject(value = "serv.mailPanel.mail_from", defaultValue="panel@miuandes.cl")
 	private String from;
-
+	@PropertyInject(value = "serv.mailPanel.mail_ccxservicio", defaultValue="")
+	private String mailCcxservicio;
+	
+	private Logger logger = Logger.getLogger(getClass());
+	
 	@Override
 	public void process(Exchange exchange) throws Exception {
 		Message message = exchange.getIn();
 		String mensaje = (String) message.getBody();
+		String operacion = (String)message.getHeader("operacion");
+		
 		@SuppressWarnings("unchecked")
 		List<Map<String, Object>> lista = (List<Map<String, Object>>)qryKcoSendmailParams.requestBody(null);
 		SendmailParamsDTO params = new SendmailParamsDTO(lista.get(0));
 		
+		// agregar al cc los mails especificos
+		String copias[] = params.getEmailsSoporte();
+		if ("crear_cuentas_AD_postgrado".equals(operacion)) {
+			copias = agregarCopias(copias, "crear_cuentas_AD_postgrado");
+		}
 		SendmailRequest sendmailRequest = new SendmailRequest(params.getEmailWebMaster(), 
-				params.getEmailsSoporte(), from, null, getAsunto(), mensaje);
+				copias, from, null, getAsunto(), mensaje);
+		logger.info(String.format("PreparaDatosMail: sendmailRequest.copias: %s", dumpCopias(copias)));
 		message.setBody(sendmailRequest);
+	}
+
+	private String dumpCopias(String[] copias) {
+		StringBuffer sb = new StringBuffer("[");
+		for (String s : copias)
+			sb.append(s).append(',');
+		if (sb.length() > 1)
+			sb.setLength(sb.length() - 1);
+		sb.append("]");
+		return sb.toString();
+	}
+
+	private String[] agregarCopias(String[] copias, String operacion) {
+		if (getMailCcxservicio() != null && getMailCcxservicio().length() > 0) {
+			Map<String,String> d = new HashMap<String,String>();
+			StringTokenizer st = new StringTokenizer(getMailCcxservicio(), "|");
+			while (st.hasMoreTokens()) {
+				String valor = st.nextToken();
+				String valores[] = valor.split(",");
+				d.put(valores[0], valores[1]);
+			}
+			String mails = d.get(operacion);
+			if (mails != null) {
+				String m[] = mails.split(";");
+				List<String> l = new ArrayList<String>();
+				for (String mm : copias) {
+					l.add(mm);
+				}
+				if (m != null && m.length > 0)
+					for (String mm : m) {
+						l.add(mm);
+					}
+				return l.toArray(new String[0]);
+			} else
+				return copias;
+		}
+		else
+			return copias;
 	}
 
 	public String getAsunto() {
@@ -54,6 +108,14 @@ public class PreparaDatosMail implements Processor {
 
 	public void setFrom(String from) {
 		this.from = from;
+	}
+
+	public String getMailCcxservicio() {
+		return mailCcxservicio;
+	}
+
+	public void setMailCcxservicio(String mailCcxservicio) {
+		this.mailCcxservicio = mailCcxservicio;
 	}
 
 }
